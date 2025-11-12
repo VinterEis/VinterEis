fe53341a527f32f187fd1e28369e1c23a8a13b78
VinterEis <coldfreezewinterboy@gmail.com>
2025-11-12 22:07:09 +0700

    commit

--- more/js/sendFeedback.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async (event, context) => {
-  const data = JSON.parse(event.body);
-
-  // For now just return the data (testing)
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: "Feedback sent!", data }),
-  };
-};
